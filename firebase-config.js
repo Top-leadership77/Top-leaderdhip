13009fab45c101e75f25206fdef80ca8ecba468b
Top-leadership77 <hichamdakhlaoui1@gmail.com>
2025-12-23 10:53:53 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase config
+firebase.initializeApp({
+  apiKey: "AIzaSyAJxzFknapSh0fnPfIXwEEK3OijSKlqxco",
+  authDomain: "admin-ash-f622c.firebaseapp.com",
+  projectId: "admin-ash-f622c",
+  storageBucket: "admin-ash-f622c.appspot.com",
+  messagingSenderId: "76505742154",
+  appId: "1:76505742154:web:dc4f190f9f2de80acb1fb3"
+});
+const db = firebase.firestore();
